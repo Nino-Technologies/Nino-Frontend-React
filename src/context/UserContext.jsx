2ec08299790bbd67548b4a8c751d59b6888b9053
Jovick-coder <victorjosiahm3@gmail.com>
2022-09-08 12:20:05 +0100

    logout function and check if user is still login

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,19 +1,47 @@
 import React, { createContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useCookies } from "react-cookie";
 
 export const UserContext = createContext();
 
 export function UserProvider({ children }) {
-  const [isLogIn, setIsLogIn] = useState(false);
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [cookies, setCookie, removeCookie] = useCookies();
+  const [userProfile, setUserProfile] = useState([]);
+  const navigate = useNavigate();
 
-  // useEffect(() => {
-  //   const currentTheme = localStorage.getItem("telecomMerchantTheme");
-  //   if (!currentTheme || currentTheme === "") {
-  //     return;
-  //   }
-  //   setTheme(currentTheme);
-  // }, [theme]);
+  useEffect(() => {
+    // console.log(cookies.grinderUser);
+    if (cookies.grinderUser === undefined) {
+      setLoggedIn(false);
+      return;
+    }
+    setLoggedIn(true);
+    setUserProfile(cookies.grinderUser.profile || null);
+  }, []);
+
+  function logOutFunction() {
+    if (window.confirm("You will be logged out of your account !!!")) {
+      localStorage.removeItem("telecomMerchant");
+
+      // setUserAccountInformation([]);
+      setUserProfile([]);
+      setLoggedIn(false);
+      removeCookie("grinderUser", { path: "/" });
+      navigate("/");
+    }
+  }
+
   return (
-    <UserContext.Provider value={{ isLogIn, setIsLogIn }}>
+    <UserContext.Provider
+      value={{
+        loggedIn,
+        setLoggedIn,
+        userProfile,
+        setUserProfile,
+        logOutFunction,
+      }}
+    >
       {children}
     </UserContext.Provider>
   );
