9ccb6ce7406000929aaaef8fa080229658b717c5
Jovick-coder <victorjosiahm3@gmail.com>
2022-09-08 12:11:58 +0100

    User login function

--- src/components/LoginForm/UserLoginForm.jsx
+++ src/components/LoginForm/UserLoginForm.jsx
@@ -1,10 +1,92 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import axios from "axios";
+import React, { useContext, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 import SocialLogin from "./SocialLogin";
+import { UserContext } from "../../context/UserContext";
+import { useCookies } from "react-cookie";
 
 function UserLoginForm() {
+  const [loading, setLoading] = useState(false);
+  const { setLoggedIn, setUserProfile } = useContext(UserContext);
+  const navigate = useNavigate();
+  const [cookies, setCookie] = useCookies();
+
+  async function loginUserFunction(e) {
+    e.preventDefault();
+    const apiUrl = "http://localhost:5000/api/auth/user/login";
+
+    const formElement = e.target;
+
+    if (formElement[0].value === "") {
+      toast.info("Email is Required");
+    }
+    if (formElement[1].value === "") {
+      toast.info("Password is Required");
+    }
+    if (formElement[1].value === "" || formElement[0] === "") {
+      return;
+    }
+
+    setLoading(true);
+    const data = {
+      email: formElement[0].value,
+      password: formElement[1].value,
+    };
+    // axios POST request
+    const options = {
+      url: apiUrl,
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json;charset=UTF-8",
+      },
+      data: data,
+    };
+
+    axios(options)
+      .then((response) => {
+        setLoading(false);
+
+        setLoggedIn(true);
+        const userToken = response.data.token;
+        const userProfile = response.data.user;
+        const cookies = {
+          profile: userProfile,
+          token: userToken,
+        };
+        setUserProfile(userProfile);
+        let expiresDate = "86400000"; // A day after
+        setCookie("grinderUser", cookies, {
+          path: "/",
+          maxAge: expiresDate,
+        });
+        toast.success("Welcome Back User");
+
+        navigate("/");
+      })
+      .catch((error) => {
+        setLoading(false);
+        console.log(error.message);
+        if (error.response.status || error.response.status === 400) {
+          return toast.error(error.response.data.message);
+        }
+        // toast.error(error.message);
+      });
+    // const rawResponse = await fetch(apiUrl, {
+    //   method: "POST",
+    //   headers: {
+    //     Accept: "application/json",
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify(data),
+    // });
+    // const content = await rawResponse.json();
+
+    // console.log(content);
+  }
   return (
-    <form class="form-layout px-5">
+    <form class="form-layout px-5" onSubmit={(e) => loginUserFunction(e)}>
       <h3 class="login-name">Login User</h3>
       <div class="">
         {/* <label for="exampleInputEmail1" class="form-label">
@@ -29,33 +111,26 @@ function UserLoginForm() {
         />
       </div>
 
-      <button type="submit" class="btn btn-primary mt-3 w-100">
-        Submit
+      <button
+        type="submit"
+        class="btn btn-primary mt-3 w-100"
+        disabled={loading}
+      >
+        {!loading ? <> Submit</> : <>Loading...</>}
       </button>
       <div class="form-text">
         <div>
-          {/* Don't have an account?{" "} */}
-          <Link to={"/login?as=artisan"}>Login As An Artisan</Link>
-          <br />
-          Don't have an account?{" "}
-          <Link to={"/register?as=user"}>Register As An User</Link>
+          Don't have an account? <br />
+          <div className="d-flex justify-content-around mt-3">
+            <Link to={"/register?as=user"} className="btn btn-outline-primary">
+              Register As An User
+            </Link>
+            <span class="my-auto">OR</span>
+            <Link to={"/login?as=artisan"} className="btn btn-outline-primary">
+              Login As An Artisan
+            </Link>{" "}
+          </div>
         </div>
-        {/* 
-        <div className="social-login-div mt-4">
-          <div className="divider d-flex flex-col umn justify-content-around">
-            <hr className="w-25 my-auto" />
-            OR
-            <hr className="w-25 my-auto" />
-          </div>
-          <div className="d-flex justify-content-around mt-2">
-            <button className="btn btn-primary mx-1">
-              <FaFacebook /> FaceBook Login
-            </button>
-            <button className="btn btn-primary mx-1">
-              <FaGoogle /> Google Login
-            </button>
-          </div>
-        </div> */}
         <SocialLogin />
       </div>
     </form>
