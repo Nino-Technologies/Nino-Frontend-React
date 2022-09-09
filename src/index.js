fa79bf2da3423b18a94e4ff53901d9c69b5e5b5d
Enahoro123 <uvieeu@gmail.com>
2022-09-09 01:20:47 +0100

    package json save

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import '@popperjs/core';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 import { UserProvider } from "./context/UserContext";
