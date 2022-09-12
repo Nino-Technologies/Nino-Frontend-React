462168d22195f6328d23d694c0013c167576b17f
Jovick-coder <victorjosiahm3@gmail.com>
2022-09-12 18:10:18 +0100

    login function

--- src/components/LoginForm/ArtisanLoginForm.jsx
+++ src/components/LoginForm/ArtisanLoginForm.jsx
@@ -1,9 +1,79 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useCookies } from "react-cookie";
+import { UserContext } from "../../context/UserContext";
+import { toast } from "react-toastify";
+import axios from "axios";
 
 function ArtisanLoginForm() {
+  const [loading, setLoading] = useState(false);
+  const { setLoggedIn, setUserProfile, apiUrl } = useContext(UserContext);
+  const [cookies, setCookie] = useCookies();
+  const navigate = useNavigate();
+
+  async function loginUserFunction(e) {
+    e.preventDefault();
+
+    const formElement = e.target;
+
+    if (formElement[0].value === "") {
+      toast.info("Email is Required");
+    }
+    if (formElement[1].value === "") {
+      toast.info("Password is Required");
+    }
+    if (formElement[1].value === "" || formElement[0] === "") {
+      return;
+    }
+
+    setLoading(true);
+    const data = {
+      email: formElement[0].value,
+      password: formElement[1].value,
+    };
+    // axios POST request
+    const options = {
+      url: `${apiUrl}/auth/artisan/login`,
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json;charset=UTF-8",
+      },
+      data: data,
+    };
+
+    axios(options)
+      .then((response) => {
+        setLoading(false);
+
+        setLoggedIn(true);
+        const userToken = response.data.token;
+        const userProfile = response.data.user;
+        const cookies = {
+          profile: userProfile,
+          token: userToken,
+        };
+        setUserProfile(userProfile);
+        let expiresDate = "86400000"; // A day after
+        setCookie("grinderUser", cookies, {
+          path: "/",
+          maxAge: expiresDate,
+        });
+        toast.success("Welcome Back Artisan");
+
+        navigate("/dashboard/home");
+      })
+      .catch((error) => {
+        setLoading(false);
+        console.log(error.message);
+        if (error.response.status || error.response.status === 400) {
+          return toast.error(error.response.data.message);
+        }
+        // toast.error(error.message);
+      });
+  }
   return (
-    <form className="form-layout px-5">
+    <form className="form-layout px-5" onSubmit={(e) => loginUserFunction(e)}>
       <h3 className="login-name">Login Artisan</h3>
       <div className="">
         {/* <label htmlFor="exampleInputEmail1" className="form-label">
@@ -30,8 +100,12 @@ function ArtisanLoginForm() {
         />
       </div>
 
-      <button type="submit" className="btn btn-primary mt-3 w-100">
-        Submit
+      <button
+        type="submit"
+        className="btn btn-primary mt-3 w-100"
+        disabled={loading}
+      >
+        {!loading ? <> Submit</> : <>Loading...</>}
       </button>
       <div className="form-text mt-3">
         <div>
