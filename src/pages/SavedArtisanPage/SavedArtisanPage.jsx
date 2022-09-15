e6b2fb3f72b8afa07b835e5a9dfe008e22178ae6
Jovick-coder <victorjosiahm3@gmail.com>
2022-09-15 22:49:22 +0100

    saved artisan page

---
+++ src/pages/SavedArtisanPage/SavedArtisanPage.jsx
@@ -0,0 +1,19 @@
+// import axios from "axios";
+import React, { useContext, useEffect, useState } from "react";
+
+import "./SavedArtisanPage.scss";
+// import AdminRegistrationForm from "../../components/RegistrationForm/AdminRegistrationForm";
+
+function SavedArtisanPage() {
+  return (
+    <div className="SavedArtisanPage pt-5">
+      <div className="header d-flex justify-content-between">
+        <h3>Saved Artisan</h3>
+      </div>
+      <hr />
+      <div className="main-div">SavedArtisanPage</div>
+    </div>
+  );
+}
+
+export default SavedArtisanPage;
