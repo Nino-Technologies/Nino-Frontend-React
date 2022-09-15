3dbfa0436d27fb2607b138cd171ddc64e1556ce1
Jovick-coder <victorjosiahm3@gmail.com>
2022-09-15 22:38:11 +0100

    conditional render of nav  link

--- src/components/Nav/dashboradNavList.js
+++ src/components/Nav/dashboradNavList.js
@@ -1,3 +1,4 @@
+import { FaCamera, FaUser, FaUserCheck, FaUserPlus } from "react-icons/fa";
 import {
   // BsAward,
   BsBellFill,
@@ -10,18 +11,42 @@ export const userNavLinkObject = [
     name: "Home",
     icon: <BsHouseFill />,
     path: "/dashboard/home",
-    userPrivilege: 1,
+    userPrivilege: 0,
   },
   {
     name: "profile",
     icon: <BsPerson />,
     path: "/dashboard/profile",
+    userPrivilege: 0,
+  },
+  {
+    name: "Upload Image",
+    icon: <FaCamera />,
+    path: "/dashboard/upload-image",
     userPrivilege: 1,
   },
   {
     name: "Notification",
     icon: <BsBellFill />,
     path: "/dashboard/notification",
-    userPrivilege: 1,
+    userPrivilege: 0,
+  },
+  {
+    name: "Users",
+    icon: <FaUser />,
+    path: "/dashboard/users",
+    userPrivilege: 3,
+  },
+  {
+    name: "Verify accounts",
+    icon: <FaUserCheck />,
+    path: "/dashboard/verify-user",
+    userPrivilege: 3,
+  },
+  {
+    name: "Create admin",
+    icon: <FaUserPlus />,
+    path: "/dashboard/create-admin",
+    userPrivilege: 3,
   },
 ];
