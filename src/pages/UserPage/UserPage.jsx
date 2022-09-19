7fed86517ef22982f3dc564bf0f1d6258d5e6f79
Jovick-coder <victorjosiahm3@gmail.com>
2022-09-19 08:23:27 +0100

    get users from api

--- src/pages/UserPage/UserPage.jsx
+++ src/pages/UserPage/UserPage.jsx
@@ -1,45 +1,97 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { useCookies } from "react-cookie";
+import { UserContext } from "../../context/UserContext";
 import "./UserPage.scss";
+import axios from "axios";
 
 function UsersPage() {
+  const [pageLoading, setPageLoading] = useState(true);
+  const [users, setUsers] = useState([]);
+  const [cookies] = useCookies();
+  useEffect(() => {
+    getUsers();
+  }, []);
+  const { apiUrl, decodeDate } = useContext(UserContext);
+  async function getUsers() {
+    const { token } = cookies.grinderUser;
+    try {
+      const resp = await axios.get(`${apiUrl}/users?role=0`, {
+        headers: {
+          authorization: token,
+        },
+      });
+      setPageLoading(false);
+      // console.log(resp.data);
+      setUsers(resp.data.data.reverse());
+    } catch (err) {
+      // Handle Error Here
+      console.error(err);
+    }
+  }
+
   return (
-    <div className="UserPage">
-      <div className="header d-flex justify-content-between">
-        <h3>Users List</h3>
+    <div className="VerifyUserPage">
+      <div className="header d-flex flex-md-row flex-column justify-content-between">
+        <h3>Verify Accounts</h3>
+        <input
+          type="text"
+          className="form-control me-4"
+          style={{ width: "200px" }}
+          placeholder="Search Account"
+        />
       </div>
       <hr />
-
-      <div class="table-responsive">
+      <div class="table-responsive-sm">
         <table class="table">
           <thead class="thead-dark">
             <tr>
-              <th scope="col">#</th>
-              <th scope="col">First</th>
-              <th scope="col">Last</th>
-              <th scope="col">Account type</th>
+              <th
+                scope="col"
+                style={{
+                  width: "50px",
+                  minWidth: "fit-content",
+                }}
+              >
+                #
+              </th>
+              <th scope="col">Full name</th>
+              <th scope="col">Email</th>
+              <th scope="col">Phone number</th>
+              <th scope="col">Date Joined</th>
               <th scope="col">Handle</th>
             </tr>
           </thead>
-          <tbody>
-            <tr>
-              <th scope="row">1</th>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
-            </tr>
-            <tr>
-              <th scope="row">2</th>
-              <td>Jacob</td>
-              <td>Thornton</td>
-              <td>@fat</td>
-            </tr>
-            <tr>
-              <th scope="row">3</th>
-              <td>Larry</td>
-              <td>the Bird</td>
-              <td>@twitter</td>
-            </tr>
-          </tbody>
+          {pageLoading ? (
+            <div className="loading">Loading....</div>
+          ) : (
+            <>
+              {" "}
+              {users.length === 0 ? (
+                <div className="loading">No user record Found</div>
+              ) : (
+                <tbody>
+                  {users.map((user, i) => {
+                    const { _id, fullName, email, phoneNumber, joinDate } =
+                      user;
+                    return (
+                      <tr key={_id}>
+                        <th scope="row">{i + 1}</th>
+                        <td>{fullName ? fullName : "-"}</td>
+                        <td>{email}</td>
+                        <td>{phoneNumber ? phoneNumber : "-"}</td>
+                        <td>{decodeDate(joinDate)[0]}</td>
+                        <td>
+                          <button className="btn btn-primary mx-auto">
+                            Action
+                          </button>
+                        </td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              )}
+            </>
+          )}
         </table>
       </div>
     </div>
