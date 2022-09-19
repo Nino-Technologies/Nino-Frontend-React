6686b6655cd2ea07b32d3ce6661be17e68f7e930
Jovick-coder <victorjosiahm3@gmail.com>
2022-09-19 08:16:12 +0100

    VerifyUserPage

--- src/pages/VerifyUserPage/VerifyUserPage.jsx
+++ src/pages/VerifyUserPage/VerifyUserPage.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { useCookies } from "react-cookie";
+import { UserContext } from "../../context/UserContext";
 import "./VerifyUserPage.scss";
+import axios from "axios";
 
 function VerifyUserPage() {
+  const [pageLoading, setPageLoading] = useState(true);
+  const [artisans, setArtisans] = useState([]);
+  const [cookies] = useCookies();
+  useEffect(() => {
+    getArtisans();
+  }, []);
+  const { apiUrl, decodeDate } = useContext(UserContext);
+  async function getArtisans() {
+    const { token } = cookies.grinderUser;
+    try {
+      const resp = await axios.get(`${apiUrl}/users?role=1`, {
+        headers: {
+          authorization: token,
+        },
+      });
+      setPageLoading(false);
+      // console.log(resp.data);
+      setArtisans(resp.data.data.reverse());
+    } catch (err) {
+      // Handle Error Here
+      console.error(err);
+    }
+  }
+
   return (
     <div className="VerifyUserPage">
       <div className="header d-flex flex-md-row flex-column justify-content-between">
@@ -14,35 +41,77 @@ function VerifyUserPage() {
         />
       </div>
       <hr />
-
       <div class="table-responsive-sm">
         <table class="table">
           <thead class="thead-dark">
             <tr>
-              <th scope="col">#</th>
+              <th
+                scope="col"
+                style={{
+                  width: "50px",
+                  minWidth: "fit-content",
+                }}
+              >
+                #
+              </th>
               <th scope="col">Full name</th>
               <th scope="col">Phone number</th>
               <th scope="col">Email</th>
               <th scope="col">NIN</th>
-              <th scope="col">Location (city, state)</th>
-              <th scope="col">Account type</th>
+              <th scope="col">
+                Location <br /> (city, state)
+              </th>
+              <th scope="col">Subscription</th>
               <th scope="col">Date Joined</th>
               <th scope="col">Handle</th>
             </tr>
           </thead>
-          <tbody>
-            <tr>
-              <th scope="row">1</th>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
-              <td>@mdo</td>
-              <td>@mdo</td>
-              <td>@mdo</td>
-              <td>@mdo</td>
-              <td>@mdo</td>
-            </tr>
-          </tbody>
+          {pageLoading ? (
+            <div className="loading">Loading....</div>
+          ) : (
+            <>
+              {" "}
+              {artisans.length === 0 ? (
+                <div className="loading">No payments record Found</div>
+              ) : (
+                <tbody>
+                  {artisans.map((artisan, i) => {
+                    const {
+                      _id,
+                      freeAccount,
+                      locationState,
+                      locationCity,
+                      status,
+                      fullName,
+                      email,
+                      phoneNumber,
+                      nin,
+                      joinDate,
+                    } = artisan;
+                    return (
+                      <tr key={_id}>
+                        <th scope="row">{i + 1}</th>
+                        <td>{fullName}</td>
+                        <td>{email}</td>
+                        <td>{phoneNumber}</td>
+                        <td>{nin ? nin : "-"}</td>
+                        <td>
+                          {locationCity}, {locationState}
+                        </td>
+                        <td>{freeAccount ? "none" : "paid"}</td>
+                        <td>{decodeDate(joinDate)[0]}</td>
+                        <td>
+                          <button className="btn btn-primary mx-auto">
+                            Action
+                          </button>
+                        </td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              )}
+            </>
+          )}
         </table>
       </div>
     </div>
