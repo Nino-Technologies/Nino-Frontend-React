ee1d3c20fb459b8d5e453ce5701bbf956af0e249
Jovick-coder <victorjosiahm3@gmail.com>
2022-09-23 08:28:04 +0100

    comment log

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -167,6 +167,7 @@ export const DashboardSideNav = ({ sideNavOpen, setSideNavOpen }) => {
 
   const { logOutFunction } = useContext(UserContext);
   const { role } = userProfile;
+  // console.log(userProfile);
   return (
     <>
       <div
