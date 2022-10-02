cfcf5dc2b5d87ddfa12ddfe923488062edba0855
Jovick-coder <victorjosiahm3@gmail.com>
2022-10-02 19:38:56 +0100

    ReviewStarComponent

--- src/components/stars/Stars.jsx
+++ src/components/stars/Stars.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./StarComponent.scss";
-import { FaStar } from "react-icons/fa";
+import { FaRegStar, FaStar } from "react-icons/fa";
 // import profilePix from "../../assets/profile-pix.jpg";
 
 function StarComponent({ rate }) {
@@ -29,3 +29,36 @@ export default StarComponent;
 //     </>
 //   );
 // }
+
+export function ReviewStarComponent({ rate, setRate }) {
+  return (
+    <div className="rating">
+      <span className="rating__result">{rate}/5</span>
+      {rate < 1 ? (
+        <FaRegStar className="rating__star" onClick={() => setRate(1)} />
+      ) : (
+        <FaStar className="rating__star" onClick={() => setRate(1)} />
+      )}
+      {rate < 2 ? (
+        <FaRegStar className="rating__star" onClick={() => setRate(2)} />
+      ) : (
+        <FaStar className="rating__star" onClick={() => setRate(2)} />
+      )}
+      {rate < 3 ? (
+        <FaRegStar className="rating__star" onClick={() => setRate(3)} />
+      ) : (
+        <FaStar className="rating__star" onClick={() => setRate(3)} />
+      )}
+      {rate < 4 ? (
+        <FaRegStar className="rating__star" onClick={() => setRate(4)} />
+      ) : (
+        <FaStar className="rating__star" onClick={() => setRate(4)} />
+      )}
+      {rate < 5 ? (
+        <FaRegStar className="rating__star" onClick={() => setRate(5)} />
+      ) : (
+        <FaStar className="rating__star" onClick={() => setRate(5)} />
+      )}
+    </div>
+  );
+}
