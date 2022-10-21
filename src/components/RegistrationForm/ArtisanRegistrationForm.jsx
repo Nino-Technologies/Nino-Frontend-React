f5fc8fb8ab93c0a0cedbed9e1e4f8162114c027c
Josiah Victor <62908527+jovick-coder@users.noreply.github.com>
2022-10-21 17:01:41 +0100

    try adding image cropper on reg

--- src/components/RegistrationForm/ArtisanRegistrationForm.jsx
+++ src/components/RegistrationForm/ArtisanRegistrationForm.jsx
@@ -4,13 +4,17 @@ import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import { UserContext } from "../../context/UserContext";
 import axios from "axios";
+import { ProfilePictureModalComponent } from "../Modal/ModalComponent";
+import ImageCropperWithStyle from "../../pages/ImageCropper/ImageCropper";
 
 function ArtisanRegistrationForm() {
   const [currentTab, setCurrentTab] = useState(1);
+  // change this to "5" to add imageCropper
   const [maxTab] = useState(4);
   const [formComplete, setFormComplete] = useState(false);
   const [loading, setLoading] = useState(false);
   // form data
+  const [avatar, setAvatar] = useState(null);
   const [email, setEmail] = useState("");
   const [fullName, setFullName] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("+234");
@@ -25,10 +29,25 @@ function ArtisanRegistrationForm() {
   // const [offersRemoteServices, setOffersRemoteServices] = useState(false);
   const [service, setService] = useState("");
   const [introduction, setIntroduction] = useState("");
+  // const [profileImage, setProfileImage] = useState(null);
+  // const [profileImageFile, setProfileImageFile] = useState(null);
 
+  // function updateProfilePicture(img) {
+  //   setProfileImage(img);
+  // }
   const { apiUrl } = useContext(UserContext);
   const navigate = useNavigate();
 
+  // useEffect(() => {
+  //   console.log(profileImage);
+  //   // console.log(blob2file(profileImage));
+  //   if (profileImage) {
+  //     const randomNumber = new Date().getTime();
+  //     const fileName = "newFIle-" + randomNumber;
+  //     setProfileImageFile(blobToFile(profileImage, fileName));
+  //     // setProfileImageFile(blob2file(profileImage));
+  //   }
+  // }, [profileImage]);
   useEffect(() => {
     if (
       email === "" ||
@@ -36,12 +55,61 @@ function ArtisanRegistrationForm() {
       phoneNumber === "" ||
       locationCity === "" ||
       locationState === "" ||
-      password === ""
+      password === "" //||
+      // avatar === ""
     ) {
       return setFormComplete(false);
     }
+
+    // if()
     setFormComplete(true);
   }, [email, fullName, phoneNumber, locationCity, locationState, password]);
+
+  // // ==================================== generate file ================================================
+
+  // function blobToFile(theBlob, fileName) {
+  //   return new File([theBlob], fileName, {
+  //     lastModified: new Date().getTime(),
+  //     type: "image/png",
+  //   });
+  // }
+
+  // function blob2file(blobData) {
+  //   const fd = new FormData();
+  //   fd.set("a", blobData);
+  //   console.log(fd.get("a"));
+  //   return fd.get("a");
+  // }
+  // // ==================================== < generate file />================================================
+  // function uploadImageToCloudinary() {
+  //   if (!profileImage || profileImage === "")
+  //     return toast.info("Image has not be edited to size");
+
+  //   if (!profileImageFile || profileImageFile === "")
+  //     return toast.info("No image selected; try again");
+
+  //   const imageData = new FormData();
+  //   imageData.append("file", profileImageFile);
+  //   imageData.append("upload_preset", "oyieaesl");
+  //   imageData.append("cloud_name", "dhvacnvek");
+
+  //   // console.log(imageData.append("file", imageFile));
+  //   // return;
+  //   console.log(profileImageFile);
+
+  //   return;
+  //   fetch("  https://api.cloudinary.com/v1_1/dhvacnvek/image/upload", {
+  //     method: "post",
+  //     body: imageData,
+  //   })
+  //     .then((resp) => resp.json())
+  //     .then((data) => {
+  //       console.log(data);
+  //       setAvatar(data.url);
+  //       // return;
+  //     })
+  //     .catch((err) => console.log(err));
+  // }
 
   async function handelSubmit(e) {
     e.preventDefault();
@@ -85,10 +153,13 @@ function ArtisanRegistrationForm() {
       return;
     }
 
+    if (!avatar || avatar === "") {
+      toast.info("Select an image before submitting");
+    }
     setLoading(true);
 
     const profileOject = {
-      avatar: "",
+      avatar: avatar,
       fullName: fullName,
       // userName: userName,
       phoneNumber: `+234${Number(phoneNumber)}`,
@@ -122,7 +193,9 @@ function ArtisanRegistrationForm() {
         setLoading(false);
 
         toast.success("Registration successfully");
-        navigate("/login?as=artisan");
+        //   navigate("/login?as=artisan");
+        // navigate("/login?as=artisan");
+        navigate(`/verify-code/${response.data.user.email}`);
         console.log(response);
       })
       .catch((error) => {
@@ -144,228 +217,254 @@ function ArtisanRegistrationForm() {
     setCurrentTab(currentTab - 1);
   }
   return (
-    <div className="ArtisanRegistrationForm">
-      {/* <!-- Circles which indicates the steps of the form: --> */}
-      <form
-        id="regForm"
-        className="form-layout"
-        onSubmit={(e) => handelSubmit(e)}
-        noValidate
-      >
-        <div style={{ textAlign: "center", marginTop: "40px" }}>
-          <ArtisanRegistrationFormIndicator
-            tabCount={maxTab}
-            activeTab={currentTab}
-            setCurrentTab={setCurrentTab}
-          />
-        </div>
-        <h3 className="login-name">Register as a Service Provider</h3>
-
-        {/* <!-- One "tab" for each step in the form: --> */}
-        <div
-          className="tab"
-          style={currentTab === 1 ? { display: "block" } : { display: "none" }}
+    <>
+      {/* <ProfilePictureModalComponent /> */}
+      <div className="ArtisanRegistrationForm">
+        {/* <!-- Circles which indicates the steps of the form: --> */}
+        <form
+          id="regForm"
+          className="form-layout"
+          onSubmit={(e) => handelSubmit(e)}
+          noValidate
         >
-          Basic Information
-          <input
-            className="form-control"
-            placeholder="Full name..."
-            type="text"
-            onChange={(e) => setFullName(e.target.value)}
-            value={fullName}
-          />
-          {/* <input
-            className="form-control"
-            placeholder="Username..."
-            type="text"
-            onChange={(e) => setUserName(e.target.value)}
-          /> */}
-          <input
-            className="form-control"
-            placeholder="Phone Number"
-            type={"number"}
-            value={phoneNumber}
-            onChange={(e) => setPhoneNumber(e.target.value)}
-          />
-          <div className="option-box-div">
-            <label className=" d-flex me-3 mt-3">
-              {" "}
-              <input
-                // className="form-control"
-                type={"radio"}
-                name="gender"
-                className="me-2"
-                onChange={(e) => setGender("Male")}
-              />{" "}
-              Male
-            </label>
-            <label className=" d-flex ms-3 mt-3">
-              <input
-                // className="form-control"
-                type={"radio"}
-                name="gender"
-                className="me-2"
-                onChange={(e) => setGender("Female")}
-              />{" "}
-              Female
-            </label>
+          <div style={{ textAlign: "center", marginTop: "40px" }}>
+            <ArtisanRegistrationFormIndicator
+              tabCount={maxTab}
+              activeTab={currentTab}
+              setCurrentTab={setCurrentTab}
+            />
           </div>
-        </div>
+          <h3 className="login-name w-75 text-center mx-auto">
+            Register as a Service Provider
+          </h3>
 
-        <div
-          className="tab"
-          style={currentTab === 2 ? { display: "block" } : { display: "none" }}
-        >
-          Location Information: <br />
-          <sup>This is use for search ranking</sup>
-          <input
-            className="form-control"
-            placeholder="City"
-            type={"text"}
-            onChange={(e) => setLocationCity(e.target.value)}
-          />
-          <input
-            className="form-control"
-            placeholder="State"
-            type={"text"}
-            onChange={(e) => setLocationState(e.target.value)}
-          />
-        </div>
-
-        <div
-          className="tab"
-          style={currentTab === 3 ? { display: "block" } : { display: "none" }}
-        >
-          Business Information:
-          <input
-            className="form-control"
-            placeholder="Service. Eg Tailor, Painter Etc"
-            type={"text"}
-            onChange={(e) => setService(e.target.value)}
-          />
-          <textarea
-            placeholder="Tell us about the service you render"
-            cols="10"
-            rows="3"
-            className="w-100 form-control"
-            onChange={(e) => setIntroduction(e.target.value)}
-          ></textarea>
-          <input
-            className="form-control"
-            placeholder="Office Location"
-            type={"text"}
-            onChange={(e) => setOfficeLocation(e.target.value)}
-          />
-          {/* <div className="option-box-div">
-            <label className=" me-3 my-2 ">
-              {" "}
+          {/* <!-- One "tab" for each step in the form: --> */}
+          <div
+            className="tab"
+            style={
+              currentTab === 1 ? { display: "block" } : { display: "none" }
+            }
+          >
+            Basic Information
+            <input
+              className="form-control"
+              placeholder="Full name..."
+              type="text"
+              onChange={(e) => setFullName(e.target.value)}
+              value={fullName}
+            />
+            <div className="d-flex">
               <input
-                // className="form-control"
-                type={"checkbox"}
-                name="gender"
-                className="me-2"
-                onChange={(e) => setDiscountsAvailable(!discountsAvailable)}
-              />{" "}
-              discounts Available
-            </label>
-            <label className="  my-2">
+                className="form-control w-25"
+                type={"text"}
+                value="+234"
+                readOnly
+              />
               <input
-                // className="form-control"
-                type={"checkbox"}
-                name="gender"
-                className="me-2"
-                onChange={(e) => setOffersRemoteServices(!offersRemoteServices)}
-              />{" "}
-              offers Remote Services
-            </label>
-          </div> */}
-        </div>
+                className="form-control"
+                placeholder="Phone Number"
+                type={"number"}
+                value={phoneNumber}
+                onChange={(e) => setPhoneNumber(e.target.value)}
+              />
+            </div>
+            <div className="option-box-div">
+              <label className=" d-flex me-3 mt-3">
+                {" "}
+                <input
+                  // className="form-control"
+                  type={"radio"}
+                  name="gender"
+                  className="me-2"
+                  onChange={(e) => setGender("Male")}
+                />{" "}
+                Male
+              </label>
+              <label className=" d-flex ms-3 mt-3">
+                <input
+                  // className="form-control"
+                  type={"radio"}
+                  name="gender"
+                  className="me-2"
+                  onChange={(e) => setGender("Female")}
+                />{" "}
+                Female
+              </label>
+            </div>
+          </div>
 
-        <div
-          className="tab"
-          style={currentTab === 4 ? { display: "block" } : { display: "none" }}
-        >
-          Login Information:
-          <input
-            className="form-control"
-            placeholder="Email"
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <input
-            className="form-control"
-            placeholder="Password..."
-            type={"password"}
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <input
-            className="form-control"
-            placeholder="Renter Password..."
-            type={"password"}
-            value={rePassword}
-            onChange={(e) => setRePassword(e.target.value)}
-          />
-          {formComplete ? null : (
-            <span className="text-danger my-2">Complete Form to Submit</span>
-          )}
-        </div>
+          <div
+            className="tab"
+            style={
+              currentTab === 2 ? { display: "block" } : { display: "none" }
+            }
+          >
+            Location Information: <br />
+            <sup>This is use for search ranking</sup>
+            <input
+              className="form-control"
+              placeholder="City"
+              type={"text"}
+              onChange={(e) => setLocationCity(e.target.value)}
+            />
+            <input
+              className="form-control"
+              placeholder="State"
+              type={"text"}
+              onChange={(e) => setLocationState(e.target.value)}
+            />
+          </div>
 
-        <div style={{ overflow: "auto" }}>
-          <div style={{ float: "right" }} className="d-flex">
-            {currentTab !== 1 ? (
-              <button
-                type="button"
-                id="prevBtn"
-                onClick={() => prevSlide()}
-                className="btn btn-primary mx-1"
-              >
-                Previous
-              </button>
-            ) : null}
-            {currentTab === maxTab ? (
-              <button
-                type="submit"
-                disabled={formComplete || loading ? false : true}
-                className="btn btn-primary mx-1"
-              >
-                {!loading ? <> Submit</> : <>Loading...</>}
-              </button>
-            ) : (
-              <button
-                type="button"
-                className="btn btn-primary mx-1"
-                id="nextBtn"
-                onClick={() => nextSlide()}
-              >
-                Next
-              </button>
+          <div
+            className="tab"
+            style={
+              currentTab === 3 ? { display: "block" } : { display: "none" }
+            }
+          >
+            Business Information:
+            <input
+              className="form-control"
+              placeholder="Service. Eg Tailor, Painter Etc"
+              type={"text"}
+              onChange={(e) => setService(e.target.value)}
+            />
+            <textarea
+              placeholder="Tell us about the service you render"
+              cols="10"
+              rows="3"
+              className="w-100 form-control"
+              onChange={(e) => setIntroduction(e.target.value)}
+            ></textarea>
+            <input
+              className="form-control"
+              placeholder="Office Location"
+              type={"text"}
+              onChange={(e) => setOfficeLocation(e.target.value)}
+            />
+          </div>
+
+          <div
+            className="tab"
+            style={
+              currentTab === 4 ? { display: "block" } : { display: "none" }
+            }
+          >
+            Login Information:
+            <input
+              className="form-control"
+              placeholder="Email"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              className="form-control"
+              placeholder="Password..."
+              type={"password"}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <input
+              className="form-control"
+              placeholder="Renter Password..."
+              type={"password"}
+              value={rePassword}
+              onChange={(e) => setRePassword(e.target.value)}
+            />
+            {formComplete ? null : (
+              <span className="text-danger my-2">Complete Form to Submit</span>
             )}
           </div>
-        </div>
-        <div className="form-text mt-3">
-          <div>
-            have an account? <br />
-            <div className="d-flex justify-content-around mt-3">
-              <Link
-                to={"/login?as=artisan"}
-                className="btn btn-outline-primary"
-              >
-                Login has a Service Provider
-              </Link>{" "}
-              {/* <span className="my-auto">OR</span>
+
+          {/* <div
+            className="tab"
+            style={
+              currentTab === 5
+                ? { display: "block", maxHeight: "650px" }
+                : { display: "none" }
+            }
+          >
+            Profile Picture
+            <div className="edit-image-register-form">
+              <ImageCropperWithStyle
+                updateProfilePicture={updateProfilePicture}
+                setProfileImageFile={setProfileImageFile}
+                profileImage={profileImage}
+              />
+              {profileImage ? (
+                <button
+                  type="button"
+                  onClick={() => {
+                    uploadImageToCloudinary();
+                  }}
+                  className="btn btn-primary mx-1 mt-4"
+                >
+                  use Image
+                </button>
+              ) : null}
+            </div>
+          </div> */}
+
+          <div style={{ overflow: "auto" }}>
+            <div style={{ float: "right" }} className="d-flex">
+              {currentTab !== 1 ? (
+                <button
+                  type="button"
+                  id="prevBtn"
+                  onClick={() => prevSlide()}
+                  className="btn btn-primary mx-1"
+                >
+                  Previous
+                </button>
+              ) : null}
+              {currentTab === maxTab ? (
+                <>
+                  {" "}
+                  {formComplete ? (
+                    <button
+                      type="submit"
+                      // disabled={formComplete || loading ? false : true}
+                      className="btn btn-primary mx-1"
+                    >
+                      {!loading ? <> Submit</> : <>Loading...</>}
+                    </button>
+                  ) : null}
+                </>
+              ) : (
+                <button
+                  type="button"
+                  className="btn btn-primary mx-1"
+                  id="nextBtn"
+                  onClick={() => nextSlide()}
+                >
+                  Next
+                </button>
+              )}
+            </div>
+          </div>
+          <div className="form-text mt-3">
+            <div>
+              have an account? <br />
+              <div className="d-flex justify-content-around mt-3">
+                <Link
+                  to={"/login?as=artisan"}
+                  className="btn btn-outline-primary"
+                >
+                  Login as a Service Provider
+                </Link>{" "}
+                {/* <span className="my-auto">OR</span>
               <Link
                 to={"/register?as=user"}
                 className="btn btn-outline-primary"
               >
                 Register User
               </Link> */}
+              </div>
             </div>
           </div>
-        </div>
-      </form>
-    </div>
+        </form>
+      </div>
+    </>
   );
 }
 
