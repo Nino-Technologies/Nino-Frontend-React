e1798da28f4d36517dbccfed09f1ea0f474df35c
Josiah Victor <62908527+jovick-coder@users.noreply.github.com>
2022-10-26 16:52:50 +0100

    created a component for header

--- src/pages/ArtisansPage/ArtisansPage.jsx
+++ src/pages/ArtisansPage/ArtisansPage.jsx
@@ -20,35 +20,16 @@ function ArtisansPage() {
     formService,
   } = useContext(SearchContext);
 
-  useEffect(() => {
-    getArtisansFunction();
-  }, []);
-  useEffect(() => {
-    ReactGa.pageview(window.location.pathname);
-  }, []);
+  // useEffect(() => {
+  //   getArtisansFunction();
+  // }, []);
+  // useEffect(() => {
+  //   ReactGa.pageview(window.location.pathname);
+  // }, []);
 
   return (
     <div className="ArtisansPage">
-      <Nav />
-
-      <div className="hero">
-        <div className="container">
-          <h3 className="hero-name">Service provider</h3>
-        </div>
-      </div>
-      <svg
-        className="svg"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 1440 320"
-      >
-        <path
-          fill="#003049"
-          fillOpacity="1"
-          d="M0,96L80,128C160,160,320,224,480,213.3C640,203,800,117,960,96C1120,75,1280,117,1360,138.7L1440,160L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"
-        ></path>
-      </svg>
-
-      <Search />
+      <ArtisansPageHeader />
       {pageLoading ? (
         <div className="container loading">Loading...</div>
       ) : (
@@ -87,3 +68,29 @@ function ArtisansPage() {
 }
 
 export default ArtisansPage;
+
+export function ArtisansPageHeader() {
+  return (
+    <>
+      {" "}
+      <Nav />
+      <div className="hero">
+        <div className="container">
+          <h3 className="hero-name">Service provider</h3>
+        </div>
+      </div>
+      <svg
+        className="svg"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 1440 320"
+      >
+        <path
+          fill="#003049"
+          fillOpacity="1"
+          d="M0,96L80,128C160,160,320,224,480,213.3C640,203,800,117,960,96C1120,75,1280,117,1360,138.7L1440,160L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"
+        ></path>
+      </svg>
+      <Search />
+    </>
+  );
+}
