7084d2dce7883026ffe48c47e5adf49449e9846c
Josiah Victor <62908527+jovick-coder@users.noreply.github.com>
2022-11-16 23:43:31 +0100

    toggleRemove function

--- src/components/SaveButton/SaveButton.jsx
+++ src/components/SaveButton/SaveButton.jsx
@@ -6,7 +6,8 @@ import "./SaveButton.scss";
 import { SaveArtisanContext } from "./../../context/saveUserContext";
 
 function SaveButton({ artisan }) {
-  const { saveArtisan, savedArtisanIds } = useContext(SaveArtisanContext);
+  const { saveArtisan, removeArtisan, savedArtisanIds } =
+    useContext(SaveArtisanContext);
   const { loggedIn, userProfile } = useContext(UserContext);
   function toggleSave() {
     if (!loggedIn) {
@@ -17,10 +18,19 @@ function SaveButton({ artisan }) {
     }
     saveArtisan(artisan);
   }
+  function toggleRemove() {
+    if (!loggedIn) {
+      return toast.info("Login to save Artisan");
+    }
+    if (userProfile.role !== 0) {
+      return toast.info("Account type can not save favorite artisan");
+    }
+    removeArtisan(artisan._id);
+  }
   return (
     <div className="SaveButton">
       {savedArtisanIds.includes(artisan._id) ? (
-        <GiHeartMinus className="icon remove" onClick={() => toggleSave()} />
+        <GiHeartMinus className="icon remove" onClick={() => toggleRemove()} />
       ) : (
         <GiHeartPlus className="icon add" onClick={() => toggleSave()} />
       )}
