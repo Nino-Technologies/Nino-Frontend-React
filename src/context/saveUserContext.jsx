b3c3e67462536641c62b8ca91c2b571835be04d8
Josiah Victor <62908527+jovick-coder@users.noreply.github.com>
2022-11-16 23:43:13 +0100

    remove artisan from favorite

--- src/context/saveUserContext.jsx
+++ src/context/saveUserContext.jsx
@@ -24,6 +24,7 @@ export function SaveArtisanProvider({ children }) {
         artisanId: artisan._id,
         avatar: artisan.avatar,
         artisanName: artisan.fullName,
+        artisanService: artisan.service,
         artisanLocation: {
           city: artisan.locationCity,
           state: artisan.locationState,
@@ -35,6 +36,36 @@ export function SaveArtisanProvider({ children }) {
       .then((response) => {
         // getProfile(id);
         toast.info("artisan added to favorite");
+        getSavedArtisanIds(response.data.userProfile.saved_artisans);
+        if (response.data.ok) {
+          setUserProfile(response.data.userProfile);
+        }
+      })
+      .catch((error) => {
+        // setLoading(false);
+        console.log(error + ".");
+        if (error.response.status || error.response.status === 400) {
+          return toast.error(error.response.data.message);
+        }
+        // toast.error(error.message);
+      });
+  }
+  function removeArtisan(id) {
+    const options = {
+      url: `${apiUrl}/favorite/remove`,
+      method: "PUT",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json;charset=UTF-8",
+        authorization: cookies.grinderUser.token,
+      },
+      data: {
+        artisanId: id,
+      },
+    };
+    axios(options)
+      .then((response) => {
+        toast.info("artisan removed from favorite");
         getSavedArtisanIds(response.data.userProfile.saved_artisans);
         if (response.data.ok) {
           setUserProfile(response.data.userProfile);
@@ -68,7 +99,9 @@ export function SaveArtisanProvider({ children }) {
   }, [userProfile]);
 
   return (
-    <SaveArtisanContext.Provider value={{ saveArtisan, savedArtisanIds }}>
+    <SaveArtisanContext.Provider
+      value={{ saveArtisan, savedArtisanIds, removeArtisan }}
+    >
       {children}
     </SaveArtisanContext.Provider>
   );
