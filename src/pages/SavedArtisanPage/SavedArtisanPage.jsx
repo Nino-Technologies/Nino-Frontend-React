6fe83774c7ea2e494a1ad1b044494c1464bba90e
Josiah Victor <62908527+jovick-coder@users.noreply.github.com>
2022-11-16 23:42:17 +0100

    show saved artisans

--- src/pages/SavedArtisanPage/SavedArtisanPage.jsx
+++ src/pages/SavedArtisanPage/SavedArtisanPage.jsx
@@ -1,15 +1,89 @@
 import React, { useContext, useEffect, useState } from "react";
+import { UserContext } from "../../context/UserContext";
 
 import "./SavedArtisanPage.scss";
+import { SaveArtisanContext } from "../../context/saveUserContext";
+import { GiHeartMinus } from "react-icons/gi";
+import { Link } from "react-router-dom";
 
 function SavedArtisanPage() {
+  const { userProfile } = useContext(UserContext);
+
+  const [savedArtisans, setSavedArtisans] = useState({
+    loading: true,
+    data: [],
+  });
+  const { removeArtisan } = useContext(SaveArtisanContext);
+  useEffect(() => {
+    if (userProfile.saved_artisans) {
+      setSavedArtisans({ loading: false, data: userProfile.saved_artisans });
+    }
+  }, [userProfile]);
+  console.log();
   return (
     <div className="SavedArtisanPage pt-5">
       <div className="header d-flex justify-content-between">
         <h3>Saved Artisan</h3>
       </div>
       <hr />
-      <div className="main-div mt-3">Page under construction</div>
+      <div className="main-div mt-3">
+        {savedArtisans.loading ? (
+          <h1>Loading...</h1>
+        ) : (
+          <>
+            {savedArtisans.data.length === 0 ? (
+              <>No saved artisan found</>
+            ) : (
+              <>
+                {savedArtisans.data.map((artisan) => (
+                  <div className="row border p-2 my-3" key={artisan.artisanId}>
+                    <div className="col-md-2">
+                      <Link to={`/artisans-profile/${artisan.artisanId}`}>
+                        <div className="image-div">
+                          <img
+                            src={`${
+                              artisan.avatar === ""
+                                ? "https://www.seekpng.com/png/detail/110-1100707_person-avatar-placeholder.png"
+                                : artisan.avatar
+                            }`}
+                            alt="Profile picture"
+                          />
+                        </div>
+                      </Link>
+                    </div>
+                    <div className="col-md-10 ">
+                      <div className="d-flex justify-content-between flex-column flex-md-row">
+                        <Link
+                          className="col w-75  w-100 p-3"
+                          to={`/artisans-profile/${artisan.artisanId}`}
+                        >
+                          {" "}
+                          <b className="d-block  ">{artisan.artisanName}</b>
+                          <span className="d-block ">
+                            <b>Service:</b> {artisan.artisanService}
+                          </span>
+                          <b>City:</b> {artisan.artisanLocation.city},{" "}
+                          <b>State: </b>
+                          {artisan.artisanLocation.state}
+                        </Link>
+                        <div className="col my-auto social-div d-flex justify-content-end">
+                          {" "}
+                          <div className="SaveButton">
+                            <GiHeartMinus
+                              className="icon remove ms-3"
+                              onClick={() => removeArtisan(artisan.artisanId)}
+                            />
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </>
+            )}
+          </>
+        )}
+      </div>
     </div>
   );
 }
