28c3ad78b7488edcb0db06f19ffbdf4304bd3c75
Josiah Victor <62908527+jovick-coder@users.noreply.github.com>
2022-11-18 13:06:56 +0100

    live api

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -16,8 +16,8 @@ export function UserProvider({ children }) {
   const [userProfile, setUserProfile] = useState([]);
   const [notification, setNotification] = useState([]);
   const navigate = useNavigate();
-  // const apiUrl = "https://nino-technologies.herokuapp.com/api";
-  const apiUrl = "http://localhost:5000/api";
+  const apiUrl = "https://nino-technologies.herokuapp.com/api";
+  // const apiUrl = "http://localhost:5000/api";
   const [pageLoading, setPageLoading] = useState(true);
 
   useEffect(() => {
