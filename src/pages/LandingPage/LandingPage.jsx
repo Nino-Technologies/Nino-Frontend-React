84cd7d37f422aa3b8d8de6469358366b0d93aaac
Josiah Victor <62908527+jovick-coder@users.noreply.github.com>
2022-12-21 13:37:29 +0100

    new landing page design

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,19 +1,37 @@
-import React, { useEffect, useContext } from "react";
+import React, { useEffect, useContext, useState } from "react";
 // import heroImage from "../../assets/images/undraw_coffee_break_h3uu.svg";
 // import smallHeroImage from "../../assets/images/hero-section-image-design-small.png";
 import "./LandingPage.scss";
 import Search from "../../components/Search/Search";
 import Nav from "../../components/Nav/Nav";
 import { Link } from "react-router-dom";
-import ReactGa from "react-ga";
 import { UserContext } from "./../../context/UserContext";
+import ProArtisanCard from "../../components/ProArtisanCard/ProArtisanCard";
+import CardCarousel from "../../components/CardCarousel/CardCarousel";
+import { SearchContext } from "./../../context/SearchContext";
+import { QuickRequestComponent } from "./../../components/Search/Search";
 
 function LandingPage() {
-  useEffect(() => {
-    ReactGa.pageview(window.location.pathname);
-  }, []);
-
   const { loggedIn } = useContext(UserContext);
+  const { artisans } = useContext(SearchContext);
+  const [proArtisanList, setProArtisanList] = useState([]);
+
+  useEffect(() => {
+    // console.log(artisans);
+    const proArtisan = [];
+    artisans.forEach((artisan) => {
+      if (
+        artisan.freeAccount === false
+        // (artisan.subscriptionExpired === false)
+      ) {
+        proArtisan.push(artisan);
+        console.log(artisan);
+      }
+    });
+
+    setProArtisanList(proArtisan);
+    console.log(proArtisan);
+  }, [artisans]);
 
   return (
     <>
@@ -40,8 +58,203 @@ function LandingPage() {
           </div>
         </div>
 
-        <div className="h-100 w-100" style={{ minHeight: "1000px" }}>
-          hi
+        <div className="artisan-request-section py-3 secondary-color">
+          <div className="artesian-request-container container">
+            <div className="row">
+              <div className="col-md-6 d-flex">
+                <div className="my-auto">
+                  <h3>Quick Request</h3>
+                  <strong>
+                    {" "}
+                    Are you looking for a service provider and you are yet to
+                    find one?
+                  </strong>{" "}
+                  <br />
+                  Lets assist in getting a trusted service provider in less than
+                  30 minutes
+                </div>
+              </div>
+              <div className="col-md-6">
+                {/* Form */}
+                <QuickRequestComponent />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="feature-section">
+          <header>
+            <h1>
+              <strong> Reliable, efficient Service providers</strong>
+              {/* <br />
+             Powered by Technology */}
+            </h1>
+
+            <p>
+              We create a link between skilled and reliable service provider and
+              their potential client.
+            </p>
+          </header>
+
+          <main>
+            <ul className="cards-container">
+              <li className="cyan-feature-card">
+                <h2>Supervisor</h2>
+                <p>Monitors activity to identify project roadblocks</p>
+                <img
+                  src="https://alfonsosuarezg.github.io/four-card-feature-section/images/icon-supervisor.svg"
+                  alt="supervisor feature icon"
+                />
+              </li>
+
+              <li className="red-feature-card">
+                <h2>Team Builder</h2>
+                <p>
+                  Scans our talent network to create the optimal team for your
+                  project
+                </p>
+                <img
+                  src="https://alfonsosuarezg.github.io/four-card-feature-section/images/icon-team-builder.svg"
+                  alt="team builder feature icon"
+                />
+              </li>
+
+              <li className="orange-feature-card">
+                <h2>Karma</h2>
+                <p>Regularly evaluates our talent to ensure quality</p>
+                <img
+                  src="https://alfonsosuarezg.github.io/four-card-feature-section/images/icon-karma.svg"
+                  alt="karma feature icon"
+                />
+              </li>
+
+              <li className="blue-feature-card">
+                <h2>Calculator</h2>
+                <p>
+                  Uses data from past projects to provide better delivery
+                  estimates
+                </p>
+                <img
+                  src="https://alfonsosuarezg.github.io/four-card-feature-section/images/icon-calculator.svg"
+                  alt="calculator feature icon"
+                />
+              </li>
+            </ul>
+          </main>
+        </div>
+        <div className="pro-artisan-section my-4 container py-4">
+          <h1 className="text-center">Pro Artisans</h1>
+
+          <CardCarousel items={proArtisanList} />
+        </div>
+        <div className="why-Choose-Us-section">
+          <section className="chooseus-section d-flex align-items-center">
+            <div className="container">
+              <div className="sec-title text-center style-two">
+                <p>Lorem Ipsum</p>
+                <h2>Why Choose Us</h2>
+              </div>
+              <div className="row clearfix">
+                <div className="col-lg-6 col-md-12 col-sm-12 content-column">
+                  <div id="content_block_04">
+                    <div className="content-box">
+                      <div className="single-item">
+                        <div className="icon-box">
+                          <div className="bg-layer"></div>
+                          <i className="fas fa-users"></i>
+                        </div>
+                        <div className="box">
+                          <h4>Lorem Ipsum</h4>
+                          <p>
+                            Lorem Ipsum is simply dummy text of the printing and
+                            typesetting industry.
+                          </p>
+                          <a href="#">
+                            <i className="fas fa-arrow-right"></i>More Details
+                          </a>
+                        </div>
+                      </div>
+                      <div className="single-item text-right">
+                        <div className="icon-box">
+                          <div className="bg-layer"></div>
+                          <i className="fas fa-box"></i>
+                        </div>
+                        <div className="box">
+                          <h4>Lorem Ipsum 2</h4>
+                          <p>
+                            Lorem Ipsum is simply dummy text of the printing and
+                            typesetting industry.
+                          </p>
+                          <a href="#">
+                            More Details<i className="fas fa-arrow-left"></i>
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-lg-6 col-md-12 col-sm-12 video-column">
+                  <div id="video_block_01">
+                    <div
+                      className="video-inner wow slideInRight"
+                      data-wow-delay="00ms"
+                      data-wow-duration="1500ms"
+                      style={{
+                        backgroundImage:
+                          "url(https://i.ibb.co/Lz7ZrL1/video-bg.jpg)",
+                      }}
+                    >
+                      <div
+                        className="pattern-layer"
+                        style={{
+                          backgroundImage:
+                            "url(https://i.ibb.co/1qq5bKr/cube-shapes.png)",
+                        }}
+                      ></div>
+                      <div className="video-btn">
+                        <div
+                          className="btn-bg rotate-me"
+                          style={{
+                            backgroundImage:
+                              "url(https://i.ibb.co/kKmvK RY/btn-icon-bg.png)",
+                          }}
+                        ></div>
+                        <a
+                          href=""
+                          className="lightbox-image"
+                          data-toggle="modal"
+                          data-target="#myModal"
+                        >
+                          <i className="fas fa-play"></i>
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+          {/* <!-- chooseus-section end --> */}
+          <div className="modal fade" id="myModal">
+            <div className="modal-dialog  modal-lg modal-dialog-centered">
+              <div className="modal-content">
+                <button type="button" className="close" data-dismiss="modal">
+                  &times;
+                </button>
+
+                {/* <!-- Modal body --> */}
+                <div className="modal-body">
+                  <iframe
+                    width="100%"
+                    height="315"
+                    src="https://www.youtube.com/embed/WqUaM-ZueXU"
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                  ></iframe>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
