0a789c21eb6dba6b849cdfb637e906ef24741a0f
Josiah Victor <62908527+jovick-coder@users.noreply.github.com>
2024-01-25 18:59:51 +0100

    remove unexpected error

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -12,21 +12,24 @@ import {
   // BsHouse,
 } from "react-icons/bs";
 import { userNavLinkObject } from "./dashboradNavList";
-// import {
-//   Avatar,
-//   Box,
-//   Button,
-//   Divider,
-//   IconButton,
-//   ListItemIcon,
-//   Menu,
-//   MenuItem,
-//   Tooltip,
-//   // Typography,
-// } from "@mui/material";
-// import { Settings, KeyboardArrowDownOutlined } from "@mui/icons-material";
+import {
+  Avatar,
+  Box,
+  Button,
+  Divider,
+  IconButton,
+  ListItemIcon,
+  Menu,
+  MenuItem,
+  Tooltip,
+  // Typography,
+} from "@mui/material";
+import { Settings, KeyboardArrowDownOutlined } from "@mui/icons-material";
 // import { styled, alpha } from "@mui/styles";
-// import { useCookies } from "react-cookie";
+import { useTheme } from "@mui/material/styles";
+import { styled } from "@material-ui/styles";
+// import { styled, alpha } from "@material-ui/styles";
+import { useCookies } from "react-cookie";
 
 const navLinks = [
   {
@@ -263,12 +266,12 @@ const StyledMenu = styled((props) => (
 ))(({ theme }) => ({
   "& .MuiPaper-root": {
     borderRadius: 6,
-    marginTop: theme.spacing(1),
+    // marginTop: theme.spacing(1),
     minWidth: 180,
-    color:
-      theme.palette.mode === "light"
-        ? "rgb(55, 65, 81)"
-        : theme.palette.grey[300],
+    // color:
+    //   theme.palette.mode === "light"
+    //     ? "rgb(55, 65, 81)"
+    //     : theme.palette.grey[300],
     boxShadow:
       "rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px",
     "& .MuiMenu-list": {
@@ -277,15 +280,15 @@ const StyledMenu = styled((props) => (
     "& .MuiMenuItem-root": {
       "& .MuiSvgIcon-root": {
         fontSize: 18,
-        color: theme.palette.text.secondary,
-        marginRight: theme.spacing(1.5),
+        // color: theme.palette.text.secondary,
+        // marginRight: theme.spacing(1.5),
       },
-      "&:active": {
-        backgroundColor: alpha(
-          theme.palette.primary.main,
-          theme.palette.action.selectedOpacity
-        ),
-      },
+      // "&:active": {
+      //   backgroundColor: alpha(
+      //     theme.palette.primary.main,
+      //     theme.palette.action.selectedOpacity
+      //   ),
+      // },
     },
   },
 }));
